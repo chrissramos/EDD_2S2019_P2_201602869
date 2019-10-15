6eabe0f62ddb79629ffc098a57034c2edf8bb9dd
chrissramos <3492193460115@ingenieria.usac.edu.gt>
2019-10-14 22:30:46 -0600

    adding server test1

--- Menu.py
+++ Menu.py
@@ -2,78 +2,103 @@ import hashlib
 from datetime import datetime
 import os
 import csv
+import socket
+import select
+import sys
+
 from Structures.DoubleLinkedList import node
 from Structures.DoubleLinkedList import DoubleLinked
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+	print ("Correct usage: script, IP address, port number")
+	exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.connect((IP_address, Port))
 exit = False
 opcion = 0
+listaDoble = DoubleLinked()
 def encrypt_string(string):
         sha_encryption = hashlib.sha256(string.encode()).hexdigest()
         return sha_encryption
-while not exit:
-    listaDoble = DoubleLinked()
-    contador = 0    
-    print ("1. Insert Block")
-    print ("2. Select Block")
-    print ("3. Reports")
-    print ("4. Salir")
-    
-    print ("Elige una opcion")
-    
-    opcion = int(input("Ingrese numero deseado: "))
-    
-    if opcion == 1:
-        os.system('cls')
-        print ("Ingrese Nombre de Archivo .csv")
-        nombreA = input()+".csv"
-        #os.system('pause')
-        print("Archivo ingresado: " + nombreA)
-        print("Leyendo archivo\n\n")
-        datos = []
-        #creando nodo bloque 
-        
-        
-        
-        #os.system("ls")
-        with open("bloques\\"+nombreA) as csvfile:
-            readCSV = csv.reader(csvfile, delimiter=',')
-            for row in readCSV:
-                #print(row)
-                #print(row[1])
-                dato = row[1]
-                datos.append(dato)
-                #print("\n")
-                #print(row[0],row[1],)
-            #datos 0 = nombre de la clase
-            #datos 1 = el json
-            #print(datos[0])
-            #print("\n")
-            #print(datos[1])
-            #agregando atributos al bloque a crear
-            if listaDoble.head is None:
-                nodoNuevo = node()
-                now = datetime.now()
-                nodoNuevo.INDEX = contador
-                nodoNuevo.PREVIOUSHASH = "0000"
-                nodoNuevo.TIMESTAMP = now.strftime("%d-%m-%Y::%H:%M:&S")
-                nodoNuevo.CLASS = datos[0]
-                nodoNuevo.DATA = datos[1]
-                nodoNuevo.HASH = encrypt_string(str(nodoNuevo.INDEX) + nodoNuevo.TIMESTAMP + nodoNuevo.CLASS + nodoNuevo.DATA +str(nodoNuevo.PREVIOUSHASH)  )
-                contador+=1
-                print("Se creo el bloque Genesis:")
-            else:
-                nodoNuevoC = node()
 
-                print("Ya tiene info")
-        os.system('pause')
+
+while True:
     
-    elif opcion == 2:
-        print ("Opcion 2")
-    elif opcion == 3:
-        print("Opcion 3")
-    elif opcion == 4:
-        exit = True
-    else:
-        print ("Introduce un numero entre 1 y 3")
- 
-print ("Fin")
+    while not exit:
+        os.system('cls')
+        
+        contador = 0    
+        print ("1. Insert Block")
+        print ("2. Select Block")
+        print ("3. Reports")
+        print ("4. Salir")
+        
+        print ("Elige una opcion")
+        
+        opcion = int(input("Ingrese numero deseado: "))
+        
+        if opcion == 1:
+            os.system('cls')
+            print ("Ingrese Nombre de Archivo .csv")
+            nombreA = input()+".csv"
+            #os.system('pause')
+            print("Archivo ingresado: " + nombreA)
+            print("Leyendo archivo\n\n")
+            datos = []
+            #creando nodo bloque 
+            
+            #os.system("ls")
+            with open("bloques\\"+nombreA) as csvfile:
+                readCSV = csv.reader(csvfile, delimiter=',')
+                for row in readCSV:
+                    #print(row)
+                    #print(row[1])
+                    dato = row[1]
+                    datos.append(dato)
+                    #print("\n")
+                    #print(row[0],row[1],)
+                #datos 0 = nombre de la clase
+                #datos 1 = el json
+                print(datos[0])
+                #print("\n")
+                #print(datos[1])
+                #agregando atributos al bloque a crear
+                if listaDoble.head is None:
+                    nodoNuevo = node()
+                    now = datetime.now()
+                    nodoNuevo.INDEX = contador
+                    nodoNuevo.PREVIOUSHASH = "0000"
+                    nodoNuevo.TIMESTAMP = now.strftime("%d-%m-%Y::%H:%M:&S")
+                    nodoNuevo.CLASS = datos[0]
+                    nodoNuevo.DATA = datos[1]
+                    nodoNuevo.HASH = encrypt_string(str(nodoNuevo.INDEX) + nodoNuevo.TIMESTAMP + nodoNuevo.CLASS + nodoNuevo.DATA +str(nodoNuevo.PREVIOUSHASH)  )
+                    listaDoble.add(nodoNuevo)
+                    contador+=1
+                    print("Se creo el bloque Genesis:")
+                else:
+                    nodoNuevoC = node()
+                    now = datetime.now()
+                    nodoNuevoC.INDEX = contador
+                    nodoNuevoC.PREVIOUSHASH = listaDoble.getUltimo().HASH
+                    nodoNuevoC.TIMESTAMP = now.strftime("%d-%m-%Y::%H:%M:&S")
+                    nodoNuevoC.CLASS = datos[0]
+                    nodoNuevoC.DATA = datos[1]
+                    nodoNuevoC.HASH = encrypt_string(str(nodoNuevoC.INDEX) + nodoNuevoC.TIMESTAMP + nodoNuevoC.CLASS + nodoNuevoC.DATA +str(nodoNuevoC.PREVIOUSHASH)  )
+                    listaDoble.add(nodoNuevoC)
+                    contador+=1
+                    print("Ya tiene info")
+            os.system('pause')
+        
+        elif opcion == 2:
+            print ("Opcion 2")
+        elif opcion == 3:
+            print("Opcion 3")
+        elif opcion == 4:
+            exit = True
+        else:
+            print ("Introduce un numero entre 1 y 3")
+    
+    
+        
--- Structures/DoubleLinkedList.py
+++ Structures/DoubleLinkedList.py
@@ -6,6 +6,8 @@ class node:
         self.DATA = None
         self.PREVIOUSHASH = None
         self.HASH = None
+        self.NEXT = None
+        self.PREVIOUS = None
 
 class DoubleLinked:
     def __init__(self):
@@ -13,3 +15,21 @@ class DoubleLinked:
 
     def returnHead(self):
         return self.head
+
+    def add(self,bloque):
+        if self.head is None:
+            self.head = bloque
+        else:
+            temp = self.head
+            while temp.NEXT is not None:
+                temp = temp.NEXT
+            temp.NEXT = bloque
+            bloque.PREVIOUS = temp
+    
+    def getUltimo(self):
+            temp = self.head
+            while temp.NEXT is not None:
+                temp = temp.NEXT
+            return temp
+           
+
