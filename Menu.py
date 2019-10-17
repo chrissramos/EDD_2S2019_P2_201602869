9716827ad7294151d996551a4ec4d46ff2b76325
chrissramos <3492193460115@ingenieria.usac.edu.gt>
2019-10-16 22:53:54 -0600

    adding menu2

--- Menu.py
+++ Menu.py
@@ -5,7 +5,7 @@ import csv
 import socket
 import select
 import sys
-
+import json
 from Structures.DoubleLinkedList import node
 from Structures.DoubleLinkedList import DoubleLinked
 
@@ -16,89 +16,120 @@ if len(sys.argv) != 3:
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 server.connect((IP_address, Port))
+#server.sendall('hola desde el cliente'.encode('utf-8'))
 exit = False
 opcion = 0
 listaDoble = DoubleLinked()
+nodoNuevo = node()
+contador = 0
 def encrypt_string(string):
         sha_encryption = hashlib.sha256(string.encode()).hexdigest()
         return sha_encryption
 
+def crearJson(index, time, clas, data, prev, has):
+    print("creando json")
+    """jsonTotal = '{'
+    jsonTotal+= "\"INDEX\": "+index
+    jsonTotal+= "\"TIMESTAMP\": "+time + ","
+    jsonTotal+= "\"CLASS\": "+clas+ ","
+    jsonTotal+= "\"DATA\": "+data + ","
+    jsonTotal+= "\"PREVIOUSHASH\": "+prev + ","
+    jsonTotal+= "\"HASH\": "+has
+    jsonTotal += '}'  
+    
+    print("Valores de variables:")
+    print(index)
+    print(time)
+    print(clas)
+    print(data)
+    print(prev)
+    print(has)
+    """
+    print("Imprimiendo solo la data")
+    print(data)
+    print("-------------------------------------")
+    #data = json.dumps(data)
+    data = data.replace("\n", "")
+    data = data.replace("'\'", "")
+    
+    objeto = {
+        "INDEX": index,
+        "TIMESTAMP": ''+time+'',
+        "CLASS": ''+clas+'',
+        "DATA": data,
+        "PREVIOUSHASH": ''+prev+'',
+        "HASH": ''+has+''
+    }
+
+    y = json.dumps(objeto)
+    print(y)
+    return y
+
 
 while True:
-    
-    while not exit:
-        os.system('cls')
-        
-        contador = 0    
-        print ("1. Insert Block")
-        print ("2. Select Block")
-        print ("3. Reports")
-        print ("4. Salir")
-        
-        print ("Elige una opcion")
-        
-        opcion = int(input("Ingrese numero deseado: "))
-        
-        if opcion == 1:
-            os.system('cls')
-            print ("Ingrese Nombre de Archivo .csv")
-            nombreA = input()+".csv"
-            #os.system('pause')
-            print("Archivo ingresado: " + nombreA)
-            print("Leyendo archivo\n\n")
-            datos = []
-            #creando nodo bloque 
-            
-            #os.system("ls")
-            with open("bloques\\"+nombreA) as csvfile:
-                readCSV = csv.reader(csvfile, delimiter=',')
-                for row in readCSV:
-                    #print(row)
-                    #print(row[1])
-                    dato = row[1]
-                    datos.append(dato)
-                    #print("\n")
-                    #print(row[0],row[1],)
-                #datos 0 = nombre de la clase
-                #datos 1 = el json
-                print(datos[0])
-                #print("\n")
-                #print(datos[1])
-                #agregando atributos al bloque a crear
-                if listaDoble.head is None:
-                    nodoNuevo = node()
+    read_sockets = select.select([server], [], [], 1)[0]
+    import msvcrt
+    if msvcrt.kbhit(): read_sockets.append(sys.stdin)
+
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            if message.decode('utf-8') != 'true' and message.decode('utf-8') != 'false' and message.decode('utf-8') != 'Welcome to [EDD]Blockchain Project!': 
+                print("Recibiendo un Json")
+                print (message.decode('utf-8'))
+                texto = json.loads(message)
+                #print(texto["CLASS"])
+            else:
+                new_message = message.decode('utf-8')
+                if new_message == 'false':
+                    print("FALSE")
+                elif new_message == 'true':
+                    print("true")
+            #print ("recv:"+message.decode('utf-8'))
+        else:
+            print("1 insert bloque")
+            print("2 select")
+            print("3 reports")
+            print("elige una opcion..")
+            opcion = int(input("Ingrese numero deseado: "))
+            if opcion == 1:
+                print ("Ingrese Nombre de Archivo .csv")
+                nombreA = input()+".csv"
+                datos = []
+                with open("bloques\\"+nombreA) as csvfile:
+                    readCSV = csv.reader(csvfile, delimiter=',')
+                    for row in readCSV:
+                        dato = row[1]
+                        datos.append(dato)
+                    archi = datos[1]
+                    
                     now = datetime.now()
                     nodoNuevo.INDEX = contador
-                    nodoNuevo.PREVIOUSHASH = "0000"
+                    if listaDoble.head is None:
+                        nodoNuevo.PREVIOUSHASH = "0000"
+                    else:
+                        nodoNuevo.PREVIOUSHASH = "0001"
                     nodoNuevo.TIMESTAMP = now.strftime("%d-%m-%Y::%H:%M:&S")
                     nodoNuevo.CLASS = datos[0]
                     nodoNuevo.DATA = datos[1]
                     nodoNuevo.HASH = encrypt_string(str(nodoNuevo.INDEX) + nodoNuevo.TIMESTAMP + nodoNuevo.CLASS + nodoNuevo.DATA +str(nodoNuevo.PREVIOUSHASH)  )
-                    listaDoble.add(nodoNuevo)
-                    contador+=1
-                    print("Se creo el bloque Genesis:")
-                else:
-                    nodoNuevoC = node()
-                    now = datetime.now()
-                    nodoNuevoC.INDEX = contador
-                    nodoNuevoC.PREVIOUSHASH = listaDoble.getUltimo().HASH
-                    nodoNuevoC.TIMESTAMP = now.strftime("%d-%m-%Y::%H:%M:&S")
-                    nodoNuevoC.CLASS = datos[0]
-                    nodoNuevoC.DATA = datos[1]
-                    nodoNuevoC.HASH = encrypt_string(str(nodoNuevoC.INDEX) + nodoNuevoC.TIMESTAMP + nodoNuevoC.CLASS + nodoNuevoC.DATA +str(nodoNuevoC.PREVIOUSHASH)  )
-                    listaDoble.add(nodoNuevoC)
-                    contador+=1
-                    print("Ya tiene info")
-            os.system('pause')
-        
-        elif opcion == 2:
-            print ("Opcion 2")
-        elif opcion == 3:
-            print("Opcion 3")
-        elif opcion == 4:
-            exit = True
-        else:
-            print ("Introduce un numero entre 1 y 3")
-    
+                    envioJsonString = crearJson(nodoNuevo.INDEX,nodoNuevo.TIMESTAMP,nodoNuevo.CLASS,nodoNuevo.DATA,nodoNuevo.PREVIOUSHASH,nodoNuevo.HASH)
+                    #jsonString = json.dumps(envioJsonString)
+                   
+                    print("******************************")
+                    print(envioJsonString)
+                    #print(nodoNuevo.DATA)
+                    #print("")
+                    #server.sendall(envioJsonString)
+
+
+                #message = sys.stdin.readline()
+                #server.sendall('JSON'.encode('utf-8'))
+                #sys.stdout.write("YOU:")
+                #sys.stdout.write(message)
+                #sys.stdout.flush()
+server.close()
+
+
     
         
